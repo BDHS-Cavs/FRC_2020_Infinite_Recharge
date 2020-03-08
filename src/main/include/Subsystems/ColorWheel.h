819d32539c67254712209cde8120bd4267564f12
beethree <beethree@gmail.com>
2020-03-08 13:30:47 -0500

    updated color values

--- src/main/include/Subsystems/ColorWheel.h
+++ src/main/include/Subsystems/ColorWheel.h
@@ -26,7 +26,7 @@ private:
   static constexpr frc::Color kBlack        = frc::Color(0, 0, 0);
   static constexpr frc::Color kBlueTarget   = frc::Color(0.143, 0.427, 0.429);
   static constexpr frc::Color kGreenTarget  = frc::Color(0.197, 0.561, 0.240);
-  static constexpr frc::Color kRedTarget    = frc::Color(0.561, 0.232, 0.114);
+  static constexpr frc::Color kRedTarget    = frc::Color(0.390, 0.409, 0.200);
   static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
 
   // Members
